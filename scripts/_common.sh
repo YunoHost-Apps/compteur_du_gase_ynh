2e9ff0f1a850a14c6515ad69b44807380fae9ac0
Félix Piédallu <felix@piedallu.me>
2024-02-28 21:49:31 +0100

    Manifest v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,35 +4,17 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-pkg_dependencies="python3-pip python3-virtualenv python3-venv python3-wheel sqlite3 build-essential python3-dev python3-dev libldap2-dev libsasl2-dev ldap-utils"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
 
-set_initial_permissions() {
-    ynh_permission_url --permission="main" --url="/"
+_venv_install() {
+    ynh_exec_as "$app" python3 -m venv --upgrade "$install_dir/venv"
+    venvpy="$install_dir/venv/bin/python3"
 
-    if ! ynh_permission_exists --permission="home_page"
-    then
-        ynh_permission_create \
-            --label "Page d\'accueil" \
-            --permission="home_page" \
-            --url="/$" \
-            --additional_urls="/activité" "/static" "/ynh_auth" \
-            --allowed="visitors"
-    fi
+    ynh_exec_as "$app" "$venvpy" -m pip install --upgrade --no-cache-dir pip
 
-    if ! ynh_permission_exists --permission="admin"
-    then
-       ynh_permission_create \
-           --label "Panneau d\'administration" \
-           --permission="admin" \
-           --allowed="$admin" \
-           --protected=true \
-           --show_tile="false"
-    fi
+    ynh_exec_as "$app" "$venvpy" -m pip install setuptools wheel pyyaml
 }
 
 #=================================================
